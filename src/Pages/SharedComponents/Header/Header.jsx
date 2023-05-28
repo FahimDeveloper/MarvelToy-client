67165c9530826326230226121bd3958386098f63
FahimDeveloper <developerfahim246@gmail.com>
2023-05-28 15:07:30 +0600

    protect privet nav when user exist or not

--- src/Pages/SharedComponents/Header/Header.jsx
+++ src/Pages/SharedComponents/Header/Header.jsx
@@ -1,8 +1,10 @@
-import { } from 'react';
+import { useContext } from 'react';
 import { NavLink } from "react-router-dom"
 import Logo from "../../../assets/logo-removebg-preview.png"
+import { AuthContext } from '../../../Auth/Auth';
 
 const Header = () => {
+    const { user } = useContext(AuthContext)
     return (
         <div className='bg-orange-50 py-2 rounded-full'>
             <div className='container mx-auto flex justify-between items-center'>
@@ -10,13 +12,20 @@ const Header = () => {
                     <img src={Logo} alt="logo" className='w-20 h-12 object-fill' />
                     <h3 className='text-3xl font-bold italic'>Marvel <span className='text-primary'>Toys</span></h3>
                 </div>
-                <div className='text-lg space-x-8 font-medium'>
+                <div className='text-xl space-x-8'>
                     <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>Home</NavLink>
                     <NavLink to="/allToys" className={({ isActive }) => isActive ? 'active' : ''}>All Toys</NavLink>
-                    <NavLink to="/sellerOwnToys" className={({ isActive }) => isActive ? 'active' : ''}>My Toys</NavLink>
-                    <NavLink to="/addToy" className={({ isActive }) => isActive ? 'active' : ''}>Add Toy</NavLink>
+                    {
+                        user ? <>
+                            <NavLink to="/sellerOwnToys" className={({ isActive }) => isActive ? 'active' : ''}>My Toys</NavLink>
+                            <NavLink to="/addToy" className={({ isActive }) => isActive ? 'active' : ''}>Add Toy</NavLink>
+                        </> : ''
+                    }
                     <NavLink to="/blog" className={({ isActive }) => isActive ? 'active' : ''}>Blog</NavLink>
-                    <NavLink to="/login" className={({ isActive }) => isActive ? 'active' : ''}>Login</NavLink>
+                    {
+                        user ? <span className='cursor-pointer'>Log Out</span>
+                            : <NavLink to="/login" className={({ isActive }) => isActive ? 'active' : ''}>Login</NavLink>
+                    }
                 </div>
             </div>
         </div>
