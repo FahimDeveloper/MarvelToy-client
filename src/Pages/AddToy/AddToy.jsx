df1e73956c36cdd263108dd036ce6302cba3c504
FahimDeveloper <developerfahim246@gmail.com>
2023-05-29 00:33:12 +0600

    change add to toy input feild and some style change done

--- src/Pages/AddToy/AddToy.jsx
+++ src/Pages/AddToy/AddToy.jsx
@@ -45,23 +45,23 @@ const AddToy = () => {
     }
     return (
         <div className='container mx-auto flex justify-center items-center min-h-screen space-y-10 py-16'>
-            <form onSubmit={handlePostProduct} className='w-4/5 mx-auto card shadow-xl space-y-5 py-5 border px-10'>
+            <form onSubmit={handlePostProduct} className='w-4/5 mx-auto card shadow-xl space-y-5 pb-5 pt-10 border px-10'>
                 <h2 className='text-center text-4xl font-medium'>Add Your Toy with all information</h2>
-                <div className='card-body space-y-3'>
+                <div className='card-body space-y-2'>
                     <div className='flex gap-5'>
-                        <div className='flex w-full flex-col gap-2'>
-                            <label htmlFor="toyName">Toy Name</label>
-                            <input type="text" name='toyname' id="toyName" className='input input-bordered' placeholder='Type name here' />
+                        <div className='flex w-full flex-col gap-1'>
+                            <label htmlFor="toyName" className='pl-2 text-sm'>Toy Name</label>
+                            <input type="text" name='toyname' id="toyName" className='input input-bordered rounded-full' placeholder='Type name here' />
                         </div>
-                        <div className='flex w-full flex-col gap-2'>
-                            <label htmlFor="toyImg">Image URL</label>
-                            <input type="url" name='photo' id="toyImg" className='input input-bordered' placeholder='Type url here' />
+                        <div className='flex w-full flex-col gap-1'>
+                            <label htmlFor="toyImg" className='pl-2 text-sm'>Image URL</label>
+                            <input type="url" name='photo' id="toyImg" className='input input-bordered rounded-full' placeholder='Type url here' />
                         </div>
                     </div>
                     <div className='flex gap-5'>
-                        <div className='flex w-full flex-col gap-2'>
-                            <label htmlFor="category">Sub Category</label>
-                            <select name="category" className='input input-bordered'>
+                        <div className='flex w-full flex-col gap-1'>
+                            <label htmlFor="category" className='pl-2 text-sm'>Sub Category</label>
+                            <select name="category" className='input input-bordered rounded-full'>
                                 <option value="" disabled>Select an option</option>
                                 <option value="ironMan">Iron Man</option>
                                 <option value="captainAmerica">Captain America</option>
@@ -71,33 +71,33 @@ const AddToy = () => {
                                 <option value="hulk">Hulk</option>
                             </select>
                         </div>
-                        <div className='flex w-full flex-col gap-2'>
-                            <label htmlFor="price">Price</label>
-                            <input type="number" name='price' id="price" className='input input-bordered' placeholder='Type price here' />
+                        <div className='flex w-full flex-col gap-1'>
+                            <label htmlFor="price" className='pl-2 text-sm'>Price</label>
+                            <input type="number" name='price' id="price" className='input input-bordered rounded-full' placeholder='Type price here' />
                         </div>
                     </div>
                     <div className='flex gap-5'>
-                        <div className='flex w-full flex-col gap-2'>
-                            <label htmlFor="sellerName">Seller Name</label>
-                            <input type="text" name='name' id="sellerName" className='input input-bordered' value={user?.displayName} />
+                        <div className='flex w-full flex-col gap-1'>
+                            <label htmlFor="sellerName" className='pl-2 text-sm'>Seller Name</label>
+                            <input type="text" name='name' id="sellerName" className='input input-bordered rounded-full' value={user?.displayName} />
                         </div>
-                        <div className='flex w-full flex-col gap-2'>
-                            <label htmlFor="sellerEmail">Seller Email</label>
-                            <input type="email" name='email' id="sellerEmail" className='input input-bordered' value={user?.email} />
+                        <div className='flex w-full flex-col gap-1'>
+                            <label htmlFor="sellerEmail" className='pl-2 text-sm'>Seller Email</label>
+                            <input type="email" name='email' id="sellerEmail" className='input input-bordered rounded-full' value={user?.email} />
                         </div>
                     </div>
                     <div className='flex gap-5'>
-                        <div className='flex w-full flex-col gap-2'>
-                            <label htmlFor="quantity">Available Quantity</label>
-                            <input type="number" name='quantity' id="quantity" className='input input-bordered' placeholder='Type quantity here' />
+                        <div className='flex w-full flex-col gap-1'>
+                            <label htmlFor="quantity" className='pl-2 text-sm'>Available Quantity</label>
+                            <input type="number" name='quantity' id="quantity" className='input input-bordered rounded-full' placeholder='Type quantity here' />
                         </div>
-                        <div className='flex w-full flex-col gap-2'>
-                            <label htmlFor="rating">Avaiblable Rating</label>
-                            <input type="number" name='rating' id="rating" className='input input-bordered' placeholder='Type price here' />
+                        <div className='flex w-full flex-col gap-1'>
+                            <label htmlFor="rating" className='pl-2 text-sm'>Avaiblable Rating</label>
+                            <input type="number" name='rating' id="rating" className='input input-bordered rounded-full' placeholder='Type price here' />
                         </div>
                     </div>
-                    <div className='flex w-full flex-col gap-2'>
-                        <label htmlFor="Description">Description</label>
+                    <div className='flex w-full flex-col gap-1'>
+                        <label htmlFor="Description" className='pl-2 text-sm'>Description</label>
                         <textarea name="description" className='border rounded-lg p-4' id="Description" rows="3" placeholder='Description'></textarea>
                     </div>
                     <button type='submit' className='btn btn-primary rounded-full'>Add Toy</button>
